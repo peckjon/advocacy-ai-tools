5a0f0809d658afb21a0a973832bbdd746d799694
Burke Holland <burkeholland@gmail.com>
2023-07-16 14:02:35 -0500

    experimenting with htmx

--- api/GetPrompts/index.js
+++ api/GetPrompts/index.js
@@ -1,26 +1,33 @@
-const cosmos = require('@azure/cosmos');
+const cosmos = require("@azure/cosmos");
 
 module.exports = async function (context, req) {
-
   try {
     // initialize a connection to the cosmos javascript client
     const { CosmosClient } = cosmos;
     const client = new CosmosClient(process.env.COSMOSDB_CONNECTION_STRING);
-    const database = client.database('YouTubeTools');
-    const container = database.container('Prompts');
+    const database = client.database("YouTubeTools");
+    const container = database.container("Prompts");
 
     // get all the items from the Prompts container
     const { resources } = await container.items.readAll().fetchAll();
 
+    // loop over the resources and compose an HTML fragement consisting of options for a select element
+    let html = "";
+    resources.forEach((prompt) => {
+      html += `<option value="${prompt.systemPrompt}">${prompt.promptName}</option>`;
+    });
+
     // return the prompts to the client
     context.res = {
-      body: resources
+      headers: {
+        "Content-Type": "text/html",
+      },
+      body: html,
     };
-  }
-  catch (error) {
+  } catch (error) {
     context.res = {
       status: 500,
-      body: error.message
+      body: error.message,
     };
   }
-}
+};
