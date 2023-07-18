59dabf3f27bdba92bb2c68e4495826403218adff
Burke Holland <burkeholland@gmail.com>
2023-07-18 15:01:27 -0500

    why err?

--- api/GetCaptions/index.js
+++ api/GetCaptions/index.js
@@ -1,20 +1,28 @@
 var { getSubtitles } = require('youtube-caption-extractor');
 
 module.exports = async function (context, req) {
-  // parse the id off the body
-  const { videoUrl } = req.body;
+  try {
+    // parse the id off the body
+    const { videoUrl } = req.body;
 
-  const videoId = videoUrl.split("v=")[1] || videoUrl.split("/").pop();
+    const videoId = videoUrl.split("v=")[1] || videoUrl.split("/").pop();
 
-  const videoCaptions = await getSubtitles({ videoID: videoId, lang: 'en' });
+    const videoCaptions = await getSubtitles({ videoID: videoId, lang: 'en' });
 
-  let formattedCaptions = "";
-  videoCaptions.forEach((caption) => {
-    formattedCaptions += `[{start: ${caption.start}}, {dur: ${caption.dur}}, {text: ${caption.text}}]\n\n`;
-  });
+    let formattedCaptions = "";
+    videoCaptions.forEach((caption) => {
+      formattedCaptions += `[{start: ${caption.start}}, {dur: ${caption.dur}}, {text: ${caption.text}}]\n\n`;
+    });
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: { captions: formattedCaptions }
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: { captions: formattedCaptions }
+    }
+  }
+  catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message
+    };
   }
 };
