f7627e547bf208619cf2ccd37e03eae36140a7f5
Your Name <youremail@example.com>
2023-07-19 13:03:16 -0500

    Got the format right for timestamps - I think

--- api/GetCaptions/index.js
+++ api/GetCaptions/index.js
@@ -1,4 +1,4 @@
-var { getSubtitles } = require('youtube-caption-extractor');
+var { getSubtitles } = require("youtube-caption-extractor");
 var fetch = require("node-fetch");
 
 module.exports = async function (context, req) {
@@ -8,22 +8,36 @@ module.exports = async function (context, req) {
 
     const videoId = videoUrl.split("v=")[1] || videoUrl.split("/").pop();
 
-    const videoCaptions = await getSubtitles({ videoID: videoId, lang: 'en' });
+    const videoCaptions = await getSubtitles({ videoID: videoId, lang: "en" });
 
     let formattedCaptions = "";
+
     videoCaptions.forEach((caption) => {
-      formattedCaptions += `[{start: ${caption.start}}, {dur: ${caption.dur}}, {text: ${caption.text}}]\n\n`;
+      // round the seconds to the nearest second
+      caption.start = Math.round(caption.start);
+
+      const formattedTime = formatTime(caption.start);
+
+      const formattedStart = formatTime(caption.start);
+      formattedCaptions += `Timestamp: ${formattedTime}, Caption: ${caption.text}\n\n`;
     });
 
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: { captions: formattedCaptions }
-    }
-  }
-  catch (error) {
+      body: { captions: formattedCaptions },
+    };
+  } catch (error) {
     context.res = {
       status: 500,
-      body: error.message
+      body: error.message,
     };
   }
 };
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
